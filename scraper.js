0aea2e6e796f726fbee1574e8e00158174d6dce7
David Jurelius <davan839@gmail.com>
2018-10-08 11:45:18 +0200

    Recursive scraping of paginated pages

--- scraper.js
+++ scraper.js
@@ -1,30 +1,44 @@
 const puppeteer = require("puppeteer");
+const devices = require("puppeteer/DeviceDescriptors");
 
 (async () => {
   const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  const url =
-    "https://marketingplatform.google.com/about/partners/find-a-partner";
-  await page.goto(url);
 
-  const titles = await page.evaluate(() =>
-    Array.from(document.querySelectorAll("div.compact h3.title"))
-      .map(partner => partner.innerText.trim())
-    )
+  const extractPartners = async function(url) {
+    const page = await browser.newPage();
+    await page.emulate(devices["iPhone 7"]);
+    await page.goto(url);
 
-  const logos = await page.evaluate(() =>
-  Array.from(document.querySelectorAll("div.compact .logo img"))
-    .map(logo => logo.src)
-  )
+    console.log(`scraping: ${url}`);
 
-  const partners = await page.evaluate(() =>
-    Array.from(document.querySelectorAll("div.compact"))
-      .map(compact => ({
-        title: compact.querySelector('h3.title').innerText.trim(),
-        logo: compact.querySelector('.logo img').src
+    const partnersOnPage = await page.evaluate(() =>
+      Array.from(document.querySelectorAll("div.compact")).map(compact => ({
+        title: compact.querySelector("h3.title").innerText.trim(),
+        logo: compact.querySelector(".logo img").src
       }))
-  )
-  console.log(partners)
-  
+    );
+    await page.close();
+
+    // recursion
+    if (partnersOnPage.length == 0) {
+      console.log(`final scrape with ${url}`);
+      // terminate
+      return partnersOnPage;
+    } else {
+      const nextPageNumber = parseInt(url.match(/page=(\d+)$/)[1], 10) + 1;
+      // go to next page
+      const nextUrl = `https://marketingplatform.google.com/about/partners/find-a-partner?page=${nextPageNumber}`;
+
+      return partnersOnPage.concat(await extractPartners(nextUrl));
+    }
+  };
+
+  const firstPage =
+    "https://marketingplatform.google.com/about/partners/find-a-partner?page=1";
+
+  const partners = await extractPartners(firstPage);
+
+  console.log(partners);
+
   await browser.close();
 })();
